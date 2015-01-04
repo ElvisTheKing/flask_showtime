75fc29e6538f43d84cee71fd5ae91af4178b6429
Sergey Konyukhovskiy <sk@onthe.net.ru>
2015-01-04 20:58:08 +0300

    due to sqlite limitations old migrations replaced with new one, that create show and episode tables all together

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 class Show(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(128))
-    slug = db.Column(db.String(64), index = True, unique = True)
+    remote_id = db.Column(db.String(64), index = True, unique = True)
 
 class Episode(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -12,6 +12,8 @@ class Episode(db.Model):
     episode = db.Column(db.Integer, nullable = False)
     show_id = db.Column(db.Integer, db.ForeignKey('show.id'), nullable = False)
     air_date = db.Column(db.Date)
+
+    db.UniqueConstraint(show_id,season,episode)
 
 
 
