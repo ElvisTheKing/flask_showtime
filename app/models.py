8c31215c3f4b4268ff4f9b2cac6e800a521b5600
Sergey Konyukhovskiy <sk@onthe.net.ru>
2015-01-04 17:41:59 +0300

    added episode model

--- app/models.py
+++ app/models.py
@@ -4,3 +4,13 @@ class Show(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(128))
     slug = db.Column(db.String(64), index = True, unique = True)
+
+class Episode(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(256))
+    season = db.Column(db.Integer)
+    episode = db.Column(db.Integer)
+    show_id = db.Column(db.Integer, db.ForeignKey('show.id'))
+
+
+
