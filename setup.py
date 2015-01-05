c023616d5f875331a1696eaaf7cac9fd300585d7
Sergey Konyukhovskiy <sk@onthe.net.ru>
2015-01-05 23:51:22 +0300

    basic packaging

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+setup(
+    name='Showtime',
+    version='0.1',
+    packages=['showtime'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "Flask",
+        "Flask-Admin",
+        "Flask-Login",
+        "Flask-Migrate",
+        "Flask-OAuthlib",
+        "Flask-Script",
+        "Flask-SQLAlchemy",
+        "pytvdbapi"
+    ],
+    entry_points = {
+        'console_scripts': [
+        ]
+    },
+    package_data={
+        'static': 'showtime/static/*',
+        'templates': 'showtime/templates/*'
+    }
+)
