96d7e72a2621860e26ad77818e132c7853a6a35f
Sergey Konyukhovskiy <sk@onthe.net.ru>
2015-01-06 19:05:34 +0300

    show autocomplete

--- showtime/admin_views.py
+++ showtime/admin_views.py
@@ -1,8 +1,13 @@
 from showtime import db,models,app
-from flask import redirect,abort,url_for
+from flask import redirect,abort,url_for,jsonify
 from flask.ext.admin import Admin,BaseView,expose,AdminIndexView
 from flask.ext.admin.contrib.sqla import ModelView
-from flask.ext.login import current_user
+from flask.ext.admin.form import rules
+from flask.ext.login import current_user,login_required
+from showtime.models import Episode,Show
+from pytvdbapi import api as tvdb_api
+tvdb_api_instance = tvdb_api.TVDB(app.config.get("TVDB_API_KEY"))
+
 
 class AuthIndexView(AdminIndexView):
     @expose('/')
@@ -16,7 +21,26 @@ class AuthModelView(ModelView):
     def is_accessible(self):
         return (not current_user.is_anonymous()) and current_user.is_admin
 
+
+class ShowView(AuthModelView):
+    form_create_rules = (
+        rules.Container('wrap', rules.Field('name')),
+        'remote_id'
+    )
+    form_edit_rules = form_create_rules
+
+    create_template = 'admin/show_create.html'
+
+@app.route('/admin/show/autocomplete/<query>.json')
+@login_required
+def autocomplete(query):
+    shows = tvdb_api_instance.search(query,'en')
+    items = map(lambda s: {"name": s.SeriesName, "id": s.seriesid, "info": s.Overview[0:150] if hasattr(s,'Overview') else ""},shows)
+
+    return jsonify(items = list(items))
+
+
 admin = Admin(app, name = "Showtime", index_view = AuthIndexView())
-admin.add_view(AuthModelView(models.Show,db.session, name = "Shows"))
+admin.add_view(ShowView(models.Show,db.session, name = "Shows"))
 admin.add_view(AuthModelView(models.Episode,db.session, name = "Episodes"))
 admin.add_view(AuthModelView(models.User,db.session, name = "Users"))
