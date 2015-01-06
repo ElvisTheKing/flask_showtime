3844f8b03ecf52860b07f75f9d960662a7104e49
Sergey Konyukhovskiy <sk@onthe.net.ru>
2015-01-06 19:09:34 +0300

    added deps pip install on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+ssh $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && env/bin/pip install -r requirements.txt"
 ssh $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && git pull origin master && /usr/bin/sv restart $DEPLOY_SERVICE"
 
