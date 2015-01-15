13b3283411016add6e2461dcb499625e7f35c29c
Sergey Konyukhovskiy <sk@onthe.net.ru>
2015-01-16 02:10:34 +0300

    migration on deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 ssh $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && git pull origin master"
 ssh $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && env/bin/pip install -r requirements.txt"
+ssh $DEPLOY_USER@$DEPLOY_HOST "cd $DEPLOY_PATH && env/bin/python ./manager.py db upgrade"
 ssh $DEPLOY_USER@$DEPLOY_HOST  "/usr/bin/sv restart $DEPLOY_SERVICE"
 
